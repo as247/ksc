3950cd9529a912844e6fef2bc309ff6598cd62f4
as247 <as247@vui360.com>
2021-10-10 21:10:22 +0700

    wait

---
+++ WinImageBuilder/assets/wait.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Waiting for installation complete...
+while [ ! -f done.txt ]; do ./spinner.sh sleep 1; done;
+echo "You're all set !!!"
--- WinImageBuilder/foreground.sh
+++ WinImageBuilder/foreground.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-
-echo Waiting for installation complete...
-while [ ! -f done.txt ]; do ./spinner.sh sleep 1; done;
-echo "You're all set !!!"
+./wait.sh
