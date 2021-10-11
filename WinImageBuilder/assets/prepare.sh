8736e0fcadd1f54e42ea72c6e3513d4dc2940652
as247 <as247@vui360.com>
2021-10-11 11:41:34 +0700

    Prepare

--- WinImageBuilder/assets/prepare.sh
+++ WinImageBuilder/assets/prepare.sh
@@ -1,7 +1,1 @@
-scp -r /root/*.sh node01:/root/
-if [ "$HOSTNAME" = controlplane ]; then
-    ssh node01
-else
-    sleep 999999
-fi
 ./wait.sh
--- WinImageBuilder/foreground.sh
+++ WinImageBuilder/foreground.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-
 while [ ! -f wait.sh ]; do sleep 1; done;
+scp -r /root/*.sh node01:/root/
+if [ "$HOSTNAME" = controlplane ]; then
+    ssh node01
+else
+    sleep 999999
+fi
 ./prepare.sh
