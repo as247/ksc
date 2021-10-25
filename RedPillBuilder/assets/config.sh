74fa7c6a7fbffd810e8aecec9787e8cdadf4e6d5
as247 <as247@vui360.com>
2021-10-25 09:43:40 +0700

    Sample

--- RedPillBuilder/assets/config.sh
+++ RedPillBuilder/assets/config.sh
@@ -2,6 +2,8 @@
 read -p "Vid: " Vid
 read -p "Pid: " Pid
 cd /root/redpill-tool-chain
+rm apollolake_user_config.json
+cp apollolake_user_config-sample.json apollolake_user_config.json
 if [ ! -z "$Vid" ]; then
 sed -i "s/0x46f4/${Vid}/g" apollolake_user_config.json
 fi
