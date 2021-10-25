e66e01c61c32085d10fe9c701ed764dda238068b
as247 <as247@vui360.com>
2021-10-25 09:51:15 +0700

    background web server

--- RedPillBuilder/assets/main.sh
+++ RedPillBuilder/assets/main.sh
@@ -8,6 +8,6 @@ while [ ! -f redpill-tool-chain.zip ]; do sleep 1; done;
 unzip redpill-tool-chain.zip
 chmod +x /root/redpill-tool-chain/*.sh
 mkdir /root/redpill-tool-chain/images
-cd /root/redpill-tool-chain/images && python3 -m http.server
+cd /root/redpill-tool-chain/images && python3 -m http.server &
 echo "You're all set !!!" > /var/run/.done
 
